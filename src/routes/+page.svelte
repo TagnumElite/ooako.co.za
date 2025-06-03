9b0a8245e29748e07a6c11c2125a41bcec377390
Tagan H <tagnum.elite@gmail.com>
2025-06-03 23:22:23 +0200

    Update the homepage to use components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,94 +1,96 @@
 <script>
-	import Quote from '$lib/Quote.svelte';
+  import '../app.css';
+
+  import Jumbotron from '$lib/Jumbotron.svelte';
+  import Quote from '$lib/Quote.svelte';
+  import Grid from '$lib/Grid.svelte';
 </script>
 
-<div class="jumbotron">
-	<div class="container">
-		<div class="title">
-			<p>Linda Ann Hoyle</p>
-			<p class="sub">1952-2024 (72 years)</p>
-		</div>
-		<div class="info">
-			<p>A mother, artist and light of the community</p>
-			<p>Someone who was loved by all and hated by none</p>
-			<p>Her creativity knew no bounds, art was her life</p>
-			<p>Her smile lit up any room, made any place feel like home</p>
-			<p></p>
-		</div>
-	</div>
-</div>
-<div class="container inline">
-	<div>
-		<Quote author="Andrew H">
-			My wife for over 50 years and my best friend through it all. She always complained about the
-			mess I left but otherwise, she has always been friendly and happy.
-		</Quote>
-	</div>
-	<div>
-		<Quote author="Tagan H">
-			I never treated granny the way a grandson should've but she still took care of me, clothed me
-			and fed me. I was never without shelter, food and clothes. She made sure everyone was taken
-			care of.
-		</Quote>
-	</div>
-	<div><Quote author="Lee H">Mom never stopped caring and worrying about me, she will be missed.</Quote></div>
-	<div>
-		<Quote author="Zara DB">
-			Your life was a masterpiece, painted with vibrant hues of love and joy. In every step we take
-			and every song we hear, your spirit dances alongside us, a beautiful reminder of the joy you
-			brought into our lives.
-		</Quote>
-	</div>
-	<div>
-		<Quote author="Jacques DB">Linda was a great mother-in-law!</Quote>
-	</div>
-</div>
-
+<Jumbotron img="/main-jumbo.png" title="Linda A. Hoyle" subtitle="1952-2024 (72 years)">
+  <p>Though brushes rest and colors fade to gray,</p>
+<p>Her vibrant soul still dances in the light.</p>
+<p>She lived her truth in every bold display,</p>
+<p>A life well-lived, now shining out of sight.</p>
+</Jumbotron>
+<!-- <div class="jumbotron">
+  <div class="container">
+    <div class="title">
+      <p>Linda Ann Hoyle</p>
+      <p class="sub">1952-2024 (72 years)</p>
+    </div>
+    <div class="info">
+      <p>A mother, artist and light of the community</p>
+      <p>Someone who was loved by all and hated by none</p>
+      <p>Her creativity knew no bounds, art was her life</p>
+      <p>Her smile lit up any room, made any place feel like home</p>
+      <p></p>
+    </div>
+  </div>
+</div> -->
+<Grid>
+  <Quote author="Andrew H">
+    My wife for over 50 years and my best friend through it all. She always complained about the
+    mess I left but otherwise, she has always been friendly and happy.
+  </Quote>
+  <Quote author="Tagan H">
+    I never treated granny the way a grandson should've but she still took care of me, clothed me
+    and fed me. I was never without shelter, food and clothes. She made sure everyone was taken care
+    of.
+  </Quote>
+  <Quote author="Lee H">Mom never stopped caring and worrying about me, she will be missed.</Quote>
+  <Quote author="Zara DB">
+    Your life was a masterpiece, painted with vibrant hues of love and joy. In every step we take
+    and every song we hear, your spirit dances alongside us, a beautiful reminder of the joy you
+    brought into our lives.
+  </Quote>
+  <Quote author="Jacques DB">Linda was a great mother-in-law!</Quote>
+</Grid>
+<!-- 
 <style lang="scss">
-	.jumbotron {
-		width: 100%;
-		background: url('/main-jumbo.png') no-repeat top;
-		background-size: 100vw;
-		text-align: center;
-		color: #e4e4e4;
-		text-shadow: 2px 2px #000;
-		border-bottom: 2px solid #000;
+  .jumbotron {
+    width: 100%;
+    background: url('/main-jumbo.png') no-repeat top;
+    background-size: 100vw;
+    text-align: center;
+    color: #e4e4e4;
+    text-shadow: 2px 2px #000;
+    border-bottom: 2px solid #000;
 
-		.container {
-			padding: 7rem 3rem;
-		}
-	}
+    .container {
+      padding: 7rem 3rem;
+    }
+  }
 
-	.container {
-		margin-left: auto;
-		margin-right: auto;
-        padding: 5px;
+  .container {
+    margin-left: auto;
+    margin-right: auto;
+    padding: 5px;
 
-		.title {
-			font-size: 4rem;
+    .title {
+      font-size: 4rem;
 
-			&:first-child p {
-				margin-bottom: 0;
-			}
+      &:first-child p {
+        margin-bottom: 0;
+      }
 
-			.sub {
-				font-size: 1.5rem;
-				margin-top: 0;
-			}
-		}
+      .sub {
+        font-size: 1.5rem;
+        margin-top: 0;
+      }
+    }
 
-		.info p {
-			text-shadow: 1px 1px #000;
-		}
+    .info p {
+      text-shadow: 1px 1px #000;
+    }
 
-		&.inline {
-			display: inline-flex;
-			flex-wrap: wrap;
-            gap: 1rem;
-
-			& * {
-				flex: 23%;
-			}
-		}
-	}
-</style>
+    //&.inline {
+    //  display: inline-flex;
+    //  flex-wrap: wrap;
+    //  gap: 1rem;
+//
+    //  & * {
+    //    flex: 23%;
+    //  }
+    //}
+  }
+</style> -->
