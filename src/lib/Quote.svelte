4c533cea460c7d7a6faffaa45114840047d15716
Tagan H <tagnum.elite@gmail.com>
2025-06-03 23:21:18 +0200

    Use tailwind on the Quououte Component

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -4,29 +4,11 @@
   export let author = '';
 </script>
 
-<blockquote>
-	<p><slot /></p>
+<blockquote
+  class="text-center p-4 my-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800 rounded shadow"
+>
+  <p class="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white"><slot /></p>
   {#if author != ''}
     <div class="pe-3 font-medium text-gray-900 dark:text-white">{author}</div>
   {/if}
 </blockquote>
-
-<style lang="scss">
-	blockquote {
-		margin: 1.5rem auto;
-		max-width: 60vw;
-		text-align: center;
-		background-color: #e7e7e7;
-		padding: 1rem;
-		border-radius: 1rem;
-
-		footer {
-			color: rgb(124, 124, 124);
-
-			&::before {
-				color: black;
-				content: '— ';
-			}
-		}
-	}
-</style>
